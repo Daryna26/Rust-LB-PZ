e160c96365f5057b090ca310d79b1142ff3d48cd
Daryna <dbondv2601@gmail.com>
2026-04-29 09:24:30 +0300

    Remove target from PZ-8

--- PZ-8-manual-future/target/debug/deps/pz_8_manual_future.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Daryna\Rust-LB-PZ\PZ-8-manual-future\target\debug\deps\pz_8_manual_future.d: src\main.rs
-
-C:\Users\Daryna\Rust-LB-PZ\PZ-8-manual-future\target\debug\deps\pz_8_manual_future.exe: src\main.rs
-
-src\main.rs:
--- PZ-8-manual-future/target/debug/pz_8_manual_future.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Daryna\Rust-LB-PZ\PZ-8-manual-future\target\debug\pz_8_manual_future.exe: C:\Users\Daryna\Rust-LB-PZ\PZ-8-manual-future\src\main.rs
